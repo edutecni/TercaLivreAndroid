dc8c72fac6090229d22a5b4ceab2abf71292f2e1
Eduardo Alves Canuto <edutecni@gmail.com>
2021-04-25 11:35:44 -0300

    Criacao 2

--- app/src/main/java/com/cursoandroid/tercalivreandroid/MainActivity.java
+++ app/src/main/java/com/cursoandroid/tercalivreandroid/MainActivity.java
@@ -4,11 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class MainActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener {
+
+    YouTubePlayerView youTubePlayerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        youTubePlayerView = (YouTubePlayerView)findViewById(R.id.yotube_view);
+    }
+
+    @Override
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+
+    }
+
+    @Override
+    public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
     }
 }
