831e1f1c73a2f9c49bc3c5a764765d642d84ccd1
Eduardo Alves Canuto <edutecni@gmail.com>
2021-04-27 07:53:50 -0300

    Terminado

--- app/src/main/java/com/cursoandroid/tercalivreandroid/MainActivity.java
+++ app/src/main/java/com/cursoandroid/tercalivreandroid/MainActivity.java
@@ -2,14 +2,18 @@ package com.cursoandroid.tercalivreandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
 import com.google.android.youtube.player.YouTubePlayerView;
 
-public class MainActivity extends AppCompatActivity implements YouTubePlayer.OnInitializedListener {
+public class MainActivity extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener, YouTubePlayer.PlaybackEventListener {
 
+    String chaveYoutube = "AIzaSyAbpAl9jAupItf2H1c5x9y5JAxDZzwmy-g";
     YouTubePlayerView youTubePlayerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,15 +21,63 @@ public class MainActivity extends AppCompatActivity implements YouTubePlayer.OnI
         setContentView(R.layout.activity_main);
 
         youTubePlayerView = (YouTubePlayerView)findViewById(R.id.yotube_view);
+        youTubePlayerView.initialize(chaveYoutube, this);
     }
 
     @Override
-    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
-
+    public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean foiRestaurado) {
+        if (!foiRestaurado)
+        {
+            youTubePlayer.cueVideo("4v-73vpBbps"); // https://youtu.be/_ce71MMDBN0
+        }
     }
 
     @Override
     public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+        if (youTubeInitializationResult.isUserRecoverableError())
+        {
+            youTubeInitializationResult.getErrorDialog(this, 1).show();
+        }
+        else
+        {
+            String error= "Erro ao inicializar o Youtube" + youTubeInitializationResult.toString();
+            Toast.makeText(getApplication(), error, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (resultCode==1){
+            getYoutubePlayerProvider().initialize(chaveYoutube, this);
+        }
+    }
+
+    protected YouTubePlayer.Provider getYoutubePlayerProvider(){
+        return youTubePlayerView;
+    }
+
+    @Override
+    public void onPlaying() {
+
+    }
+
+    @Override
+    public void onPaused() {
+
+    }
+
+    @Override
+    public void onStopped() {
+
+    }
+
+    @Override
+    public void onBuffering(boolean b) {
+
+    }
+
+    @Override
+    public void onSeekTo(int i) {
 
     }
 }
